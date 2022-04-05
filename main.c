af62af133a1089a90e8f966c90206ad4fee9332a
Jakov Krnic <jakkr195@student.liu.se>
2022-04-05 15:53:42 +0200

    Manual control

--- main.c
+++ main.c
@@ -2,6 +2,10 @@
 #include <avr/interrupt.h>
 #include <math.h>
 
+#include "../communication-module/common/avr_i2c.h"
+#include "../communication-module/common/i2c_common.h"
+
+
 void PWM_init() {
 	// Set fast PWM mode with non-inverted output for engine
 	TCCR0A = (1<<WGM00) | (1<<WGM01) |  (1<<COM0A1);
@@ -13,61 +17,102 @@ void PWM_init() {
 	
 	DDRB |= 0x08; // Output port engine (Yellow wire)
 	DDRD |= (1<<5); // Output port steering (Green wire)
-	ICR1 = 20000; // Servo frequency
+	
 	sei();        // Enable interrupt
+	
+	ICR1 = 20000; // Servo frequency
+	OCR0A = 0;    // Initial speed
+	OCR1A = 1590; // Initial steering output
 }
 
 
-// Limit the speed to max 100 (of 255)
-int limit_speed(int speed) {
+/* Ensures the speed stays within reasonable bounds (100 < OCR0A < 0, max OCR0A = 255) */
+void set_speed(int16_t speed) {
 	if (speed > 100) {
-		return 100;
+		OCR0A = 100;
+	} else if (speed < 0) {
+		OCR0A = 0;
 	} else {
-		return speed;
+		OCR0A = speed;
 	}
 }
 
+
+/* Ensures the steering angle stays within bounds (1230 < OCR1A < 1950). Neutral when OCR1A = 1590 */
+void set_steering(int16_t steering) {  // uint16_t didn't work.
+	if (1590 + steering < 1230) {
+		OCR1A = 1230;
+	} else if (1590 + steering > 1950) {
+		OCR1A = 1950;
+	} else {
+		OCR1A = 1590 + steering;
+	}
+}
 
 
 /*  steering control
 	r_lat: lateral distance to reference point from the closest wall
 	l_lat: the cars current lateral distance from the closest wall
-	cur_speed: the cars current speed
+	cur_speed (v): the cars current speed
 	cur_angle: the cars current angle relative to the road
 */
-int set_steering_pwm(int r_lat, int l_lat, int cur_speed, int cur_angle) {
+void set_steering_pwm(int r_lat, int l_lat, int cur_speed, int cur_angle) {
 	uint8_t KP = 1;  // Proportional control variable
 	uint8_t KD = 1;  // Derivative control variable
 	uint16_t y = KP*(r_lat - l_lat) + KD*(cur_speed*sin(cur_angle));
-	if (y < -360) {
-		return -360;
-	} else if (y > 360) {
-		return 360;
-	} else {
-		return y;
-	}
+	set_steering(y);
 }
 
 
-/*  Set speed and angle values
-	The car is in neutral mode when OCR1A = 1590
-	Maximal left-turn the car is capable of occurs at OCR1A = 1590 + (-360)
-	Maximal right-turn the car is capable of occurs at OCR1A = 1590 + 360
-	The above is regulated by set_steering_pwm
-*/
-
-void PWM_SET(int steering_angle, int throttle) {
-	OCR0A = limit_speed(throttle);					// Engine duty cycle
-	OCR1A = 1590 + set_steering_pwm(100, 80, steering_angle, 0);			// Steering duty cycle
-}
-
-
-int main ()
-{
+int main() {
 	PWM_init();
+	I2C_init();
+	
+	uint16_t message_names[16];
+	uint16_t messages[16];
 
-	while (1)
-	{	
-		PWM_SET(-360, 0);
+	while (1) {	
+		
+		if (i2c_new_data) {
+			i2c_new_data = false;
+			int len = I2C_unpack(message_names, messages); 
+			
+			bool man_gas = false;
+			bool man_steer = false;
+			
+			for (int i=0; i<len; ++i) {
+				switch (message_names[i]) {
+					
+					case 0xfff0:
+					//case STEERING.MANUAL_GAS:
+						man_gas = true;
+						break;
+					case 0xfff1:
+					//case STEERING.MANUAL_ANG:
+						man_steer = true;
+						break;
+						
+					default:
+						break;
+				}
+			}
+			
+			if (man_gas && man_steer) {
+				// Manual mode
+				for (int i=0; i<len; ++i) {
+					switch (message_names[i]) {
+						
+						case 0xfff0:
+						//case STEERING.MANUAL_GAS:
+							set_speed(messages[i]);
+							break;
+						case 0xfff1:
+						//case STEERING.MANUAL_ANG:
+							set_steering(restore_signed(messages[i]));
+							break;
+					}
+				}
+			}
+		}
 	}
 }
